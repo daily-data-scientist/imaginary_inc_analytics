8c826bde05af0fa241d79d44484740c3466e49cd
daily-data-scientist <learnanalyticswithme@gmail.com>
2022-09-22 11:40:15 +0700

    update for video #2 intro to data model

---
+++ README.md
@@ -0,0 +1,7 @@
+## **Hello Data Scientists!**
+
+Welcome to the Github repository of Daily Data Scientist :)
+
+This repository will stay in sync with the videos being uploaded on our YouTube channel. It will have all the code and supporting documentation.
+
+Subscribe to the channel and help build this community! Thanks.
--- analytics_app/global.sql
+++ analytics_app/global.sql
@@ -1,10 +1,13 @@
---Global MRR
+--Global
 
 select 
 	date_trunc('month',rs.month)::date as month     --casting to date is important because the date range filter expects 'date' type
 	,sum(revenue) as mrr 
+	,sum(s.nb_seats) as nb_seats
+	,count(distinct s.id_customer) as nb_customers 
 from revenue_schedule rs 
+left join subscriptions s on s.id = rs.id_subscription 
 where 1 = 1
 and date_trunc('month',rs.month) <= date_trunc('month', now())  
 group by 1
-order by 1 desc 
+order by 1 desc
